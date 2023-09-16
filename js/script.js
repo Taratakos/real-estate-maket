abda35e0e62ddc9d60c2753568b6cf753e7f7fb2
Taras Zarytskyi <taras.zarytskyi@gmail.com>
2023-09-16 06:13:45 +0300

    fix: adjust js for tabs

--- js/script.js
+++ js/script.js
@@ -9,12 +9,26 @@ const swiper = new Swiper(".slider-main-block", {
 });
 
 // Таби
-
+const tabNavItems = document.querySelectorAll(".tabs-deals__button");
+const tabItems = document.querySelectorAll(".item-tabs");
 // 1-й етап це делагування, відслідковування натиску/кліку на об'єкт
 document.addEventListener("click", function (e) {
   const targetElement = e.target;
-  // відсіюємо
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+  // відсіюємо елементи
   if (targetElement.closest(".tabs-deals__button")) {
-    console.log("Yes!");
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains("active")) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove("active");
+      }
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+    });
+    targetElement.classList.add("active");
+    tabItems[currentActiveIndex].classList.remove("active");
+    tabItems[newActiveIndex].classList.add("active");
   }
 });
